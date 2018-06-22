418bb33a80ccbb645080aee94c2084518e0e899b
Sean Altemose <sean@seanaltemose.com>
2018-06-22 19:59:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,6 +37,7 @@ class Student
     student = Student.new(name, grade)
     student.save
     student
+  end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
